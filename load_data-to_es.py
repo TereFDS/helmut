6c9696ad1a7a9d0a54a64f7b3dc0908265fad0f2
Teresa Fontanella De Santis <tere.fds@hotmail.com>
2018-02-10 10:29:13 -0300

    Get list of files to upload to ES

--- load_data-to_es.py
+++ load_data-to_es.py
@@ -1,1 +1,29 @@
-import elasticsearch
+import sys
+import os
+
+from elasticsearch import Elasticsearch
+
+
+def get_files_list(path='./data', extension='.txt'):
+    """
+    Returns list of files with a given extension
+
+    :param path:
+    :param extension:
+
+    :returns List of relative file_names
+    """
+    directory = os.scandir(path)
+    files_list = []
+    for dirc in directory:
+        if dirc.is_file():
+            lala, extension1 = os.path.splitext(dirc.name)
+            if extension1 == extension:
+                files_list.append(dirc.name)
+    return files_list
+
+
+if __name__ == "__main__":
+    directory = get_files_list()
+    import pdb; pdb.set_trace()
+    es = Elasticsearch(host='localhost', port=9200)
